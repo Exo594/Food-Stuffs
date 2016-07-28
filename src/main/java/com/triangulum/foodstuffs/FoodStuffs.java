aef6114eb8154d4c7e5e73bc49899642d0766ce8
Sylla-bus <syllabus319@gmail.com>
2016-07-28 16:12:30 -0600

    Initial Commit to create the mod environment, just clone into your own copy of the thing.

---
+++ src/main/java/com/triangulum/foodstuffs/FoodStuffs.java
@@ -0,0 +1,9 @@
+package com.triangulum.foodstuffs;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = "FoodStuffs", name = "Food Stuffs", version = "TEST")
+public class FoodStuffs
+{
+
+}
